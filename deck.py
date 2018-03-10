80ace8f1d6b865ea7f03d82bac3b7ee8eb1541fb
HarrisonHall <harrisonchristianhall@gmail.com>
2018-03-10 17:08:01 -0500

    fixes

--- deck.py
+++ deck.py
@@ -2,6 +2,7 @@
 import itertools
 import random
 
+'''
 class Player(object):
 	name = "" #string
 	money = 0 #int
@@ -10,20 +11,24 @@ class Player(object):
 	def __init__(name):
 		name.name = name
 		name.money = 100
+'''
 
 class card(object):
 		def __init__(self ,rank, suit):
 			self.rank = rank #number
 			self.suit = suit #character
-
-mdeck = [card(rank, suit) for
-		rank, suit in itertools.product(
-			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-			['C', 'D', 'H', 'S'])
-		]
+def makedeck():
+        mdeck = [card(rank, suit) for
+		 rank, suit in itertools.product(
+			 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+			 ['C', 'D', 'H', 'S'])
+        ]
+        return mdeck
 
 def shuffle(deck):
 	random.shuffle(deck)
+        return deck
 
 def draw(deck):
 	deck.pop()
+        return deck
