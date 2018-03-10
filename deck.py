96ab0dedfe537e8e8b14dd41bbcc8de7ba1cc1aa
JackieADoan <jackiedoan10@gmail.com>
2018-03-10 14:48:55 -0500

    Made the deck and cards

--- deck.py
+++ deck.py
@@ -1,1 +1,29 @@
 #deck.py
+import itertools
+import random
+
+class Player(object):
+	name = "" #string
+	money = 0 #int
+	phand = [] #array
+
+	def __init__(name):
+		name.name = name
+		name.money = 100
+
+class card(object):
+		def __init__(self ,rank, suit):
+			self.rank = rank #number
+			self.suit = suit #character
+
+mdeck = [card(rank, suit) for
+		rank, suit in itertools.product(
+			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+			['C', 'D', 'H', 'S'])
+		]
+
+def shuffle(deck):
+	random.shuffle(deck)
+
+def draw(deck):
+	deck.pop()
